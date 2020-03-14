36c3306c7598e159913e73e75ddcbba3e5e33e14
iHad169 <27337315+iHad169@users.noreply.github.com>
2020-03-14 18:47:09 +0800

    init

---
+++ ParameterShower/Form1.Designer.cs
@@ -0,0 +1,26 @@
+﻿namespace ParameterShower
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ ParameterShower/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ParameterShower
+{
+    public partial class Form1 : Form
+    {
+        private string[] args;
+
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(259, 230);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Multiline = true;
+            foreach (string arg in args) {
+                this.textBox1.Text += arg + Environment.NewLine;
+            }
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+        public Form1(string[] args)
+        {
+            this.args = args;
+            InitializeComponent();
+        }
+    }
+}
